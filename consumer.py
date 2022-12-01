a3c6bc293107cfb05c434611690aaa53e9758f92
Pedro Drehmer <108018065+PedroTDrehmer@users.noreply.github.com>
2022-12-01 11:30:32 -0300

    Add files via upload

---
+++ consumer.py
@@ -0,0 +1,16 @@
+import time
+from kafka import KafkaConsumer
+import json
+
+if __name__ == "__main__":
+    consumer = KafkaConsumer(
+        "topic name",
+        bootstrap_servers = '192.168.1.160:22222',
+        auto_offset_reset = 'earliest',
+        enable_auto_commit = True,
+        group_id = "consumer-group-a")
+    print('kafka initiated')
+
+for msg in consumer:
+    print("Registered User = {}".format(json.loads(msg.value)))
+    time.sleep(10)
---
+++ producer.py
@@ -0,0 +1,17 @@
+from kafka import KafkaProducer
+import json
+from data import get_registered_user
+import time
+
+def json_serializer(data):
+    return json.dumps(data).encode("utf-8")
+
+producer = KafkaProducer(bootstrap_servers=['192.168.1.160:2222'],
+                         value_serializer=json_serializer)
+
+if __name__ == "__main__":
+    while 1 == 1:
+        registered_user = get_registered_user()
+        print(registered_user)
+        producer.send("registered_user", registered_user)
+        time.sleep(4)
